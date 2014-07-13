75e6e0efe65154b787e82cd41d774ab2ae66d933
VDON <y630357146@gmail.com>
2014-07-13 22:16:05 +0800

    Wikioi

--- 1203 判断浮点数是否相等/Run_297552_Score_33_Date_2013-10-20.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-double a,b;
-scanf("%lf %lf",&a,&b);
-if(abs(a-b)<=0.000000001)printf("yes");
-else printf("no");
-return 0;
-}
--- 3281 计算面积（子程序入门）/Run_405941_Score_100_Date_2013-12-21.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a,b;
-    cin>>a>>b;
-    int s=3*a*a-3*b*b;
-    cout << s;
-    return 0;
-}
